d53164fc6d604881dd26a08bb709053c5bdc70d8
Ludovic Nuel <ludovicnuel38110@gmail.com>
2026-01-26 14:43:58 +0100

    Remove broken files

--- models/races.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("../config/db");
-
-const raceSchema = new mongoose.Schema({
-  name: String,
-  date: Date,
-  horses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Horse" }]
-});
-
-module.exports = mongoose.model("Race", raceSchema);
