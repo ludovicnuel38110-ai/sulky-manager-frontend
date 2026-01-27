1a3b2250b93b1137a9189ca95d1bd660faed309f
Ludovic Nuel <ludovicnuel38110@gmail.com>
2026-01-27 14:59:05 +0100

    Enable auth and admin routes

--- server.js
+++ server.js
@@ -4,22 +4,19 @@ require("./config/db");
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// ================= ROUTES =================
+// Routes
+app.use("/api/races", require("./routes/races"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/admin", require("./routes/admin"));
-app.use("/api/races", require("./routes/races"));
 
-// Test route
 app.get("/", (req, res) => {
-  res.send("Sulky Bet API is running");
+  res.send("Sulky-Bet Backend OK");
 });
 
-// ================= SERVER =================
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("✅ Server running on port", PORT);
+  console.log("Server running on port", PORT);
 });
